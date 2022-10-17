977701870833acc589f6b64c039ba9c3453aef24
Štefan Malček <stefan.malcek1@gmail.com>
2022-10-17 20:43:15 +0200

    Extend client config.

--- src/backends/identity-service/Settings/IdentityServerSettings.cs
+++ src/backends/identity-service/Settings/IdentityServerSettings.cs
@@ -5,9 +5,10 @@ namespace FoodDelivery.IdentityService.WebApi.Settings;
 public class IdentityServerSettings
 {
     public IReadOnlyCollection<ApiScope> ApiScopes { get; set; } = Array.Empty<ApiScope>();
-    public IReadOnlyCollection<Client> Clients { get; set; }
+    public IReadOnlyCollection<Client> Clients { get; init; }
     public IReadOnlyCollection<IdentityResource> IdentityResources { get; set; } = new IdentityResource[]
     {
-        new IdentityResources.OpenId()
+        new IdentityResources.OpenId(),
+        new IdentityResources.Profile()
     };
 }
